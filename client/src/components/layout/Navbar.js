af6ce6e577de1e19282a43b723ff368ed87614f4
ryanmwakio <ryanmwakio6@gmail.com>
2021-07-24 14:05:12 +0300

    initial commit

---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+      
+        <nav className="navbar">
+            <h1>
+                <a href="index.html" className="nav-brand">RNET</a>
+            </h1>
+            <ul>
+                <li><a href="profiles.html">Developers</a></li>
+                <li><a href="register.html">Register</a></li>
+                <li><a href="login.html">Login</a></li>
+            </ul>
+        </nav>
+       
+    );
+}
+
+export default Navbar;
+
+
